015445af68aee60679b4c22d8e2566344a57d506
8Nero <tugu@mailbox.unideb.hu>
2023-11-17 01:25:13 +0100

    added gitignore files

--- include/configs.h
+++
@@ -1,9 +0,0 @@
-//=============PINs=================//
-#define I2C_SDA   21
-#define I2C_SCL   22
-#define PHOTOPIN  33 // Photoresistor pin
-
-//==================================//
-// Network configuration
-const char* ssid     = "ASUS";
-const char* password = "Jeremias777";
